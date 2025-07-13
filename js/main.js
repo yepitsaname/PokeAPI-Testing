6556662e695be552ab2a51985992e3505e6475dd
yepitsaname <fernando.g.berry@gmail.com>
2025-07-13 00:55:32 -0400

    completes a rough draft of pokemon cards and ensures images can load in them

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ const Player = {
 
 Pokemon.Get('Exeggutor');
 Pokemon.Get('Diglett');
+
+let aThing = (()=>{
+  var done = false;
+  var img = [false, false];
+
+  return () => {
+    if( Pokemon.pokemon.hasOwnProperty('diglett') && !img[0] ){
+      document.querySelector("#party-slot-one .party-image").src = Pokemon.pokemon.diglett.sprites.front_default;
+      img[0] = true;
+      console.log("Diglett loaded");
+    }
+    if( Pokemon.pokemon.hasOwnProperty('exeggutor') && !img[1] ){
+      document.querySelector("#party-slot-two .party-image").src = Pokemon.pokemon.exeggutor.sprites.front_default;
+      img[1] = true;
+      console.log("Exeggutor loaded");
+    }
+    if( img[0] && img[1] ){
+      clearInterval(tempInterval)
+    }
+  }
+})()
+
+let tempInterval = setInterval(aThing, 1000)
